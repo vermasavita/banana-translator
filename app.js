e0bea213be33b745a50340ed34559761e5bd7001
Savita Verma <svitaverma10@gmail.com>
2021-08-25 11:21:34 +0530

    fix bug

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
 
-console.log(txtInput)
+console.log(txtInput);
 
 function clickHandler() {
     console.log("Clicked");
+    console.log("input", txtInput.value);
 };
 
 btnTranslate.addEventListener("click", clickHandler)
