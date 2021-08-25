caf85af9f17a95745f9fa27f542e5396982ef326
Savita Verma <svitaverma10@gmail.com>
2021-08-25 15:10:18 +0530

    refactor:add fuctionalites

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
 
-console.log(txtInput);
+function getTranslationURL(text) {
+    return serverUrl + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.logI("error occured", error);
+    alert("Something went wrong with server! try again after sometime")
+
+}
 
 function clickHandler() {
-    console.log("Clicked");
-    console.log("input", txtInput.value);
+    var inputText = txtInput.value;
+
+    //calling server for processing
+
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
